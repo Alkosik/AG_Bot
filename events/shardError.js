de96667227dcf3fe3b9bf0eb12ca2b079d0d144a
Alkosik <alkos.yt@gmail.com>
2021-09-07 16:45:15 +0200

    Sharding error handling, error event fix

--- events/shardError.js
+++ events/shardError.js
@@ -2,12 +2,12 @@ const { MessageEmbed } = require('discord.js');
 const chalk = require('chalk');
 
 module.exports = {
-	name: 'error',
+	name: 'shardError',
 	execute(client, error) {
 		const testChannelId = '879456954232209508';
-		console.log(chalk.red('ERROR'), `An error has been encountered: ${error}`);
+		console.log(chalk.red('SHARD ERROR'), `A shard error has been encountered: ${error}`);
 		const errEmbed = new MessageEmbed()
-			.setTitle('Error Detected')
+			.setTitle('Shard Error Detected')
 			.setColor('RED');
 		client.channels.cache.get(testChannelId).send({ embeds: [errEmbed] });
 	},
