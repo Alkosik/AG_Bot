7148349541e07ce12c159c7418322362c4d7066f
Alkosik <alkos.yt@gmail.com>
2021-09-08 17:07:02 +0200

    status update

--- events/ready.js
+++ events/ready.js
@@ -7,7 +7,7 @@ module.exports = {
 		console.log(chalk.greenBright('INIT INFO'), 'Connected to Discord as ' + client.user.tag);
 
 		console.log(chalk.greenBright('INIT INFO'), 'Setting an activity.');
-		client.user.setPresence({ activities: [{ name: '34+35' }], status: 'online' });
+		client.user.setPresence({ activities: [{ name: '34+35', type: 'COMPETING' }], status: 'online' });
 		console.log(chalk.greenBright('INIT INFO'), `Activity set to: ${client.user.presence.activities[0].name}`);
 	},
 };
