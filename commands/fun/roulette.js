c875e8e1e34b823c4207a55b2f67e07fad86ef57
Alkosik <alkos.yt@gmail.com>
2021-09-11 22:59:26 +0200

    Preparation for 4-player roulette

--- commands/fun/roulette.js
+++ commands/fun/roulette.js
@@ -1,59 +1,97 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageActionRow, MessageButton } = require('discord.js');
+const wait = require('util').promisify(setTimeout);
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('rr')
-		.setDescription('Russian roulette.'),
+		.setDescription('Russian roulette but with a friend.')
+		.addUserOption(option =>
+			option.setName('partner')
+				.setDescription('Partner do duo')
+				.setRequired(true)),
 	async execute(interaction) {
-		// return interaction.reply('Ruletka zostala chwilowo wylaczona.');
-		const result = Math.random() * (8 - 1) + 1;
+
+		// Emoji
+		const gunLeft = interaction.client.emojis.cache.find(emoji => emoji.name === 'gun');
+		const gunRight = interaction.client.emojis.cache.find(emoji => emoji.name === 'gunright');
+
+		// Player 1
 		const player = interaction.member;
-		const playerUser = interaction.user;
-		let reply;
 
-		if (player.id == '284366115348414466') {
-			return interaction.reply('Your use of this commands has been suspended because of abusive usage.');
+		// Player 2
+		const duoPlayer = interaction.options.getMember('partner');
+
+		// RR result
+		const result = Math.random() * (15 - 1) + 1;
+
+		if (duoPlayer.user.id == player.id) {
+			return interaction.reply('Nie możesz grać sam ze sobą.');
 		}
 
-		switch (Math.floor(result)) {
-		case 1:
-			if (player.kickable) {
-				player.ban();
-				reply = 'Banicja';
-			} else {
-				return interaction.reply('kurwo nie mam permisji');
-			}
-			break;
-		case 2:
-			if (player.kickable) {
-				player.kick();
-				reply = 'Kop na klate';
-			} else {
-				return interaction.reply('kurwo nie mam permisji');
-			}
-			break;
-		case 3:
-			if (player.voice.sessionId == null) {
-				return interaction.reply('kurwo nie ma cie na kanale');
-			} else {
-				player.voice.setMute(true);
-				reply = 'Wyciszenie';
-			}
-			break;
-		case 4:
-			if (player.voice.sessionId == null) {
-				return interaction.reply('kurwo nie ma cie na kanale');
-			} else {
-				player.voice.setDeaf(true);
-				reply = 'Ogluszenie';
-			}
-			break;
-		default:
-			return interaction.reply('Wygrałeś.');
-		}
 
-		playerUser.send(reply);
-		console.log(Math.floor(result));
-		await interaction.reply(reply);
+		// Invitation
+		const filter = i => i.customId === 'rraccept' && i.user.id === duoPlayer.user.id;
+		const collector = interaction.channel.createMessageComponentCollector({ filter, time: 15000 });
+
+		const inviteRow = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setCustomId('rraccept')
+					.setLabel('Akceptuj')
+					.setStyle('DANGER')
+					.setEmoji(gunLeft),
+			);
+
+		await interaction.reply({ content: `${duoPlayer}, Zostałeś zaproszony do zagrania w ruletkę z ${player.user.username}`, components: [inviteRow] });
+
+		// const accpetEmbed = new MessageEmbed()
+		// 	.setTitle('Zaproszenie zaakceptowane.')
+		// 	.setColor('GREEN');
+		await collector.on('collect', async i => {
+			if (i.customId === 'rraccept') {
+				await interaction.editReply({ content: 'Zaproszenie zaakceptowane.', components: [] });
+			}
+			StartGame();
+		});
+
+		async function StartGame() {
+			let stepped = true;
+			await wait(1000);
+			await interaction.editReply('Rozpoczynanie gry.');
+			await wait(1000);
+			await interaction.editReply('Rozpoczynanie gry..');
+			await wait(1000);
+			await interaction.editReply('Rozpoczynanie gry...');
+			await wait(1000);
+			for (let step = 0; step < result; step++) {
+				await wait(200);
+				if (stepped == true) {
+					await interaction.editReply(`${player} ${gunLeft} ${duoPlayer}`);
+					stepped = false;
+				} else {
+					await interaction.editReply(`${player} ${gunRight} ${duoPlayer}`);
+					stepped = true;
+				}
+			}
+			if (stepped == false) {
+				await interaction.followUp(`${duoPlayer} wygrał. Kickowanie ${player}.`);
+				if (!player.kickable) {
+					await interaction.followUp('jebac was');
+				} else {
+					player.send('https://discord.gg/7XcYkDU');
+					player.kick();
+				}
+			} else {
+				await interaction.followUp(`${player} wygrał. Kickownie ${duoPlayer}`);
+				if (!duoPlayer.kickable) {
+					await interaction.followUp('jebac was');
+				} else {
+					duoPlayer.send('https://discord.gg/7XcYkDU');
+					duoPlayer.kick();
+				}
+			}
+		}
+		// await interaction.reply({ content: `Zostałeś zaproszony do zagrania w ruletkę z ${player.user.username}`, components: [inviteRow] });
 	},
 };
