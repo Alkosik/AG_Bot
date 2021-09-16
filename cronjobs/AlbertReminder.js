c8e396953b18943638b41eb3c352cca3c9955dbd
Alkosik <alkos.yt@gmail.com>
2021-09-16 17:17:57 +0200

    placeholded emoji fix

--- cronjobs/AlbertReminder.js
+++ cronjobs/AlbertReminder.js
@@ -2,21 +2,21 @@ const cron = require('node-schedule');
 
 module.exports = (config, client, chalk) => {
 	const main_channel_id = config.mainChannelId;
-	cron.scheduleJob('1 1 * * *', function() {
+	cron.scheduleJob('* * * * *', function() {
 		(async () => {
 			console.log(chalk.green('CRON INFO'), 'Initiating Albert\'s Reminder.');
 			const janus = client.emojis.cache.find(emoji => emoji.name === 'JanusChamp');
-			const pepo_love = client.emojis.cache.find(emoji => emoji.name === 'PepoLove');
+			const pepo_love = client.emojis.cache.find(emoji => emoji.name === 'peepoLove');
 
 			const mood = Math.random() * (20 - 1) + 1;
 			const moodFloored = Math.floor(mood);
+
 			if (moodFloored >= 10) {
 				client.channels.cache.get(main_channel_id).send(`<@430140838345965595>, kocham cie ${pepo_love}`);
 			} else if (moodFloored < 3) {
 				client.channels.cache.get(main_channel_id).send(`<@430140838345965595>, kocham cie ${pepo_love} ~ Kacperek`);
 			} else {
 				client.channels.cache.get(main_channel_id).send(`<@430140838345965595>, nienawidze cie ${janus}`);
-
 			}
 		})();
 	});
