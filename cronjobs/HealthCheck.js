79772759d5c4fc4755c9ceea35189a7bb78a4a6f
Alkosik <alkos.yt@gmail.com>
2021-10-21 22:25:31 +0200

    Added self-checking

---
+++ cronjobs/HealthCheck.js
@@ -0,0 +1,33 @@
+const cron = require('node-schedule');
+
+const { MessageEmbed } = require('discord.js');
+
+const errorCount = 0;
+
+let title;
+
+module.exports = (config, client, chalk) => {
+	const channelId = config.testChannelId;
+	cron.scheduleJob('0 0 * * *', function() {
+		(async () => {
+			console.log(chalk.green('CRON INFO'), 'Initiating Health Check.');
+
+			if (errorCount > 0) {
+				title = 'Functional';
+			} else {
+				title = 'Needs attention';
+			}
+
+			const statusEmbed = new MessageEmbed()
+				.setAuthor('Routine Health Check')
+				.setTitle(title)
+				.setColor('#00e031')
+				.setFooter('Gang Słoni 2.0', 'https://i.ibb.co/rk0Z6Mb/Grupfdgggdrszga-1.png')
+				.addFields(
+					{ name: 'Errors', value: errorCount, inline: true },
+				);
+
+			client.channels.cache.get(channelId).send({ embeds: [statusEmbed] });
+		})();
+	});
+};
