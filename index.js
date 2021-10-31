49bc5cd3656c17496df82dc2f9fd5955994383dd
Alkosik <alkos.yt@gmail.com>
2021-10-31 12:10:27 +0100

    Better webhooks

--- index.js
+++ index.js
@@ -232,8 +232,15 @@ server.listen(process.env.PORT, () => {
 
 app.post('/webhook', async (req, res) => {
 	const Payload = req.body;
+	let webhook_response;
 	// Respond To Heroku Webhook
 	res.sendStatus(200);
+
+	if (Payload.action == 'create') {
+		webhook_response = `A new buld was created for **${Payload.data.app.name}** on behalf of **${Payload.data.user.email}** with the ID **${Payload.data.id}**`;
+	} else if (Payload.action == 'update' && Payload.data.status == 'succeeded') {
+		webhook_response = `Last build of ${Payload.data.app.name} finished with status **${Payload.data.status}**, creating release version **${Payload.data.release.version}**`;
+	}
 
 	const options = {
 		method: 'POST',
@@ -245,7 +252,7 @@ app.post('/webhook', async (req, res) => {
 		// Format JSON DATA
 		body: JSON.stringify({
 			// content: `A new build/release for **${Payload.data.app.name}** detected`,
-			content: Payload.action == 'create' ? `A new buld was created for **${Payload.data.app.name}** on behalf of **${Payload.data.user.email}** with the ID **${Payload.data.id}**` : 'You fucked something up B)',
+			content: webhook_response,
 		}),
 	};
 	request(options, function(error, response) {
