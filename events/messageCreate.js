07c8ef4766f241a4df635b9c15025e186380365e
Alkosik <alkos.yt@gmail.com>
2021-11-22 22:45:38 +0100

    hot

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -12,7 +12,7 @@ const formattedDate = date.getFullYear() + '/' + (date.getMonth() + 1) + '/' + d
 module.exports = {
 	name: 'messageCreate',
 	execute(message, client, connection) {
-		connection.query(`SELECT * FROM stats WHERE date = ${formattedDate}`, function(err, rows) {
+		connection.query(`SELECT * FROM stats`, function(err, rows) {
 			if (err) {
 				client.channels.cache.get(config.testChannelId).send('**A database error detected**');
 				throw err;
