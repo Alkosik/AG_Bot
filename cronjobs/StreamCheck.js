4238a146f0fbce75c574b8e4828c8770dc4e94c8
Alkosik <alkos.yt@gmail.com>
2022-01-04 22:43:11 +0100

    Better strim check

--- cronjobs/StreamCheck.js
+++ cronjobs/StreamCheck.js
@@ -5,8 +5,10 @@ const cron = require('node-schedule');
 const { ApiClient } = require('@twurple/api');
 const { ClientCredentialsAuthProvider } = require('@twurple/auth');
 
+const { MessageEmbed } = require('discord.js');
+
 module.exports = (config, client, chalk) => {
-	cron.scheduleJob('*/2 * * * *', async function() {
+	cron.scheduleJob('* * * * *', async function() {
 		const clientId = process.env.API_TWITCH_ID;
 		const clientSecret = process.env.API_TWITCH_SECRET;
 		// const tokenData = JSON.parse(await fs.promises.readFile('./tokens.json'));
@@ -24,11 +26,21 @@ module.exports = (config, client, chalk) => {
 		}
 
 		if (await isStreamLive('grigori_rzannikov')) {
-			client.channels.cache.get(config.announceChannelId).send('Grigori ma strima! https://www.twitch.tv/grigori_rzannikov');
+			const user = await apiClient.users.getUserByName('grigori_rzannikov');
+			const stream = await user.getStream();
+
+			const streamEmbed = new MessageEmbed()
+				.setAuthor('Strim jest kurwa - grigori_rzannikov')
+				.setColor('#ffffff')
+				.setFooter('Gang Słoni 2.0', 'https://i.ibb.co/rk0Z6Mb/Grupfdgggdrszga-1.png')
+				.setTimestamp()
+				.setThumbnail(stream.getThumbnailUrl(128, 128))
+				.setDescription('https://www.twitch.tv/grigori_rzannikov')
+				.setTitle(stream.title);
+			client.channels.cache.get(config.announceChannelId).send({ embeds: [streamEmbed] });
 			console.log(chalk.green('CRON INFO'), 'Grigori is live!');
 		} else {
 			return;
-			// console.log(chalk.red('CRON INFO'), 'Grigori is not live!');
 		}
 	});
 };
