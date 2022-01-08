5eb8c45d38fe475ed513e8489116698ac35b81c1
Alkosik <alkos.yt@gmail.com>
2022-01-08 11:22:59 +0100

    Fix stats collection

--- cronjobs/ServerStatsCollector.js
+++ cronjobs/ServerStatsCollector.js
@@ -5,10 +5,12 @@ const monitor = new cronitor.Monitor('Discord Server Stats Collector');
 module.exports = (config, client, chalk, connection) => {
 	cron.scheduleJob('0 0 * * *', async function() {
 		console.log(chalk.green('CRON INFO'), 'Collecting Server Stats...');
+		client.channels.cache.get(config.testChannelId).send('Initated Server Stats and Info Collection');
 		const guilds = await client.guilds.cache.map(guild => guild.id);
 		// console.log(guilds);
 		for (const guild of guilds) {
 			console.log(chalk.green('CRON INFO'), `Collecting stats for ${guild}`);
+			client.channels.cache.get(config.testChannelId).send(`Collecting stats for ${guild}`);
 			const guildObj = client.guilds.cache.get(guild);
 			const guild_id = guildObj.id;
 			const guild_name = guildObj.name;
@@ -33,7 +35,7 @@ module.exports = (config, client, chalk, connection) => {
 						}
 					});
 				} else {
-					connection.query(`UPDATE server SET name = ${guild_name}, icon_url = ${guild_icon}, owner = ${guild_owner}, owner_id = ${guild_owner_id}, member_count = ${guild_member_count}, member_count_human = ${guild_member_count_human}, message_count = ${guild_message_count}, description = ${guild_description} WHERE id = ${guild}`, function(err) {
+					connection.query(`UPDATE server SET name = "${guild_name}", icon_url = "${guild_icon}", owner = "${guild_owner}", owner_id = ${guild_owner_id}, member_count = ${guild_member_count}, member_count_human = ${guild_member_count_human}, message_count = ${guild_message_count}, description = ${guild_description} WHERE id = ${guild}`, function(err) {
 						if (err) {
 							client.emit('error', err);
 							throw err;
@@ -41,6 +43,8 @@ module.exports = (config, client, chalk, connection) => {
 					});
 				}
 			});
+			console.log(chalk.green('CRON INFO'), `Stats collected for ${guild}`);
+			client.channels.cache.get(config.testChannelId).send(`Stats collected for ${guild}`);
 		}
 		monitor.ping({ message: 'Stats Collected' });
 	});
