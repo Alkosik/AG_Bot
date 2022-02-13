8d64e4d43fef7a938d252d0fed33a41bc4d70b6f
Alkosik <alkos.yt@gmail.com>
2022-02-13 15:20:29 +0100

    pt. 2

--- modules/webserver.js
+++ modules/webserver.js
@@ -186,7 +186,7 @@ app.post('/webhook', async (req, res) => {
 			webhook_response = `A new buld was created for **${Payload.data.app.name}** on behalf of **${Payload.data.user.email}** with the ID **${Payload.data.id}**`;
 		} else if (Payload.action == 'update' && Payload.data.status == 'succeeded') {
 			embed_name = `Build success - ${Payload.data.app.name}`;
-			webhook_response = `Last build of **${Payload.data.app.name}** finished with status **${Payload.data.status}**` + Payload.data.realease.version ? ` creating release version **${Payload.data.release.version}**` : '';
+			webhook_response = `Last build of **${Payload.data.app.name}** finished with status **${Payload.data.status}**` + Payload.data.realease ? ` creating release version **${Payload.data.release.version}**` : '';
 		} else {
 			embed_name = 'Build failure';
 			webhook_response = 'The build **failed**, just like you.';
