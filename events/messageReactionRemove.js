4b88cc6ccfe161b6560232e8497702640c95267f
Alkosik <alkos.yt@gmail.com>
2022-02-17 10:24:49 +0100

    Rection roles

---
+++ events/messageReactionRemove.js
@@ -0,0 +1,30 @@
+const chalk = require('chalk');
+const config = require('../config.json');
+
+module.exports = {
+	name: 'messageReactionRemove',
+	async execute(reaction, user, client) {
+		if (reaction.partial) {
+			try {
+				await reaction.fetch();
+			} catch (error) {
+				console.error(chalk.redBright('EVENT ERROR'), 'Something went wrong when fetching the message:', error);
+				return;
+			}
+		}
+
+
+		if (reaction.message.channelId === '943621480095313930' && reaction.message.id === '943626841724452884') {
+			const guild = client.guilds.cache.get(config.vstGuildId);
+			const member = guild.members.cache.get(user.id);
+
+			if (reaction.emoji.name === '🎙️') {
+				member.roles.remove(guild.roles.cache.find(r => r.id === '943787009250586634'));
+				console.log(chalk.green('EVENT'), `Removed role rapper from ${user.username}#${user.discriminator}`);
+			} else if (reaction.emoji.name === '🎹') {
+				member.roles.remove(guild.roles.cache.find(r => r.id === '943787088610996244'));
+				console.log(chalk.green('EVENT'), `Removed role producer from ${user.username}#${user.discriminator}`);
+			}
+		}
+	},
+};
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ exports.config = config;
 // + Discord
 const { Client, Collection, Intents, MessageEmbed } = require('discord.js');
 const myIntents = new Intents();
-myIntents.add([Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.DIRECT_MESSAGES]);
+myIntents.add([Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS]);
 const client = new Client({
 	partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
 	intents: myIntents,
