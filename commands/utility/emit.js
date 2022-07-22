a3a012eb437e3516d5cca9a4c133296777383d28
Alkosik <alkos.yt@gmail.com>
2022-07-22 10:06:21 +0200

    Minor v14 compatibility, interaction handling in events

--- commands/utility/emit.js
+++ commands/utility/emit.js
@@ -1,16 +1,18 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder, Options } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('emit')
 		.setDescription('Emits an event.')
-		.addStringOption(option =>
-			option.setName('event')
-				.setDescription('Event')
-				.setRequired(true)
-				.addChoice('memAdd', 'guildMemberAdd')
-				.addChoice('memRemove', 'guildMemberRemove')
-				.addChoice('interactionCreate', 'interactionCreate')),
+		.addOption(option => options: [
+				name: 'event',
+				description: 'Event',
+				required: true,
+				type: ApplicationCommandOptionType.String,
+			],
+				// .addChoice('memAdd', 'guildMemberAdd')
+				// .addChoice('memRemove', 'guildMemberRemove')
+				// .addChoice('interactionCreate', 'interactionCreate')),
 	async execute(interaction) {
 		if (!interaction.member.roles.cache.some(r => r.name === '♚Słonie♚')) {
 			await interaction.reply('Co ty kurwa chcesz zrobic powiedz ty mi');
