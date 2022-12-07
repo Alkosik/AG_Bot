83447c006aa37be0075d958a915506c9072a4309
Alkosik <alkos.yt@gmail.com>
2022-12-07 18:52:45 +0100

    add config to event args

--- index.js
+++ index.js
@@ -99,9 +99,9 @@ const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'
 for (const file of eventFiles) {
 	const event = require(`./events/${file}`);
 	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
+		client.once(event.name, (...args) => event.execute(...args, config));
 	} else {
-		client.on(event.name, (...args) => event.execute(...args, client));
+		client.on(event.name, (...args) => event.execute(...args, client, config));
 	}
 }
 // #endregion
