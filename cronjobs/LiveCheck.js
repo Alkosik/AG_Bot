8131512eec2b908b72144eac3156b253fc0fd98a
Alkosik <alkos.yt@gmail.com>
2023-07-16 13:36:51 +0200

    Made Live Check not spit out errors

--- cronjobs/LiveCheck.js
+++ cronjobs/LiveCheck.js
@@ -1,32 +1,35 @@
-const cron = require('node-schedule');
-const { google } = require('googleapis');
-const index = require('../index');
+const cron = require("node-schedule");
+const { google } = require("googleapis");
+const index = require("../index");
 
 module.exports = (config, client, chalk) => {
-	console.log(chalk.green('CRON INFO'), 'Live Check starting');
-	cron.scheduleJob('*/30 * * * *', function() {
-		console.log(chalk.green('CRON INFO'), 'Checking for live...');
-		// Check if a youtube channel is live
-		const youtube = google.youtube({
-			version: 'v3',
-			auth: process.env.API_YOUTUBE_KEY,
-		});
+  console.log(chalk.green("CRON INFO"), "Live Check starting");
+  cron.scheduleJob("*/30 * * * *", function () {
+    console.log(chalk.green("CRON INFO"), "Checking for live...");
+    // Check if a youtube channel is live
+    const youtube = google.youtube({
+      version: "v3",
+      auth: process.env.API_YOUTUBE_KEY,
+    });
 
-		youtube.search.list({
-			part: 'snippet',
-			channelId: 'UCXc1c6bs1-pLECw90LYLL4A',
-			type: 'video',
-			eventType: 'live',
-		}, (err, data) => {
-			if (err) {
-				console.log(chalk.red('CRON ERROR'), 'Error checking for live:', err);
-			} else if (data.data.items[0] == undefined) {
-				console.log(chalk.red('CRON ERROR'), 'No live detected');
-				index.stream = false;
-			} else if (data.data.items[0].snippet.liveBroadcastContent === 'live') {
-				console.log(chalk.green('CRON INFO'), 'Live detected');
-				index.stream = true;
-			}
-		});
-	});
+    youtube.search.list(
+      {
+        part: "snippet",
+        channelId: "UCXc1c6bs1-pLECw90LYLL4A",
+        type: "video",
+        eventType: "live",
+      },
+      (err, data) => {
+        if (err) {
+          console.log(chalk.red("CRON ERROR"), "Error checking for live:", err);
+        } else if (data.data.items[0] == undefined) {
+          console.log(chalk.yellow("CRON INFO"), "No live detected");
+          index.stream = false;
+        } else if (data.data.items[0].snippet.liveBroadcastContent === "live") {
+          console.log(chalk.green("CRON INFO"), "Live detected");
+          index.stream = true;
+        }
+      }
+    );
+  });
 };
